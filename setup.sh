0e7ca23f946ade47eedf1bb027525006d2ea4485
me <sholto>
2021-12-23 07:32:26 +0000

    useful

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository ppa:longsleep/golang-backports -y
+sudo apt update
+sudo apt install golang-go
+env GOPATH=/root/go
+sudo apt-get install graphviz gv
+go install github.com/google/pprof@latest
+pip install jupyter
