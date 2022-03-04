e8cec77d1c8e555c5d32aa795949e16952846c95
Sholto <sholto.douglas1@gmail.com>
2022-03-04 12:29:44 +1100

    Update setupTPU.sh

--- model_parallel_tutorial/setupTPU.sh
+++ model_parallel_tutorial/setupTPU.sh
@@ -1,3 +1,3 @@
 
-gcloud alpha compute tpus tpu-vm create lfp1 --zone=us-central1-f --accelerator-type=v3-8 --version=v2-alpha --project learning-from-play-303306
+gcloud alpha compute tpus tpu-vm create lfp1 --zone=us-central1-f --accelerator-type=v2-8 --version=v2-alpha --project learning-from-play-303306
 gcloud alpha compute tpus tpu-vm ssh lfp1 --zone=us-central1-f --project learning-from-play-303306 -- -L 8888:localhost:8888
